550f6133fc0b69dedc9b8468468d7338fd899033
meshramamrapali <amrapalimeshram86@gmail.com>
2023-02-24 21:56:54 +0530

    ADDED : Check The Position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,10 +2,30 @@ package SnakeAndLadder;
 
 public class SnakeAndLadder {
     static final int PLAYER_POSITION = 0;
-    public static void main(String[] args) {
-        System.out.println("Welcome to Snake And Ladder Game");
-        int dice = (int) (Math.random() * 10) % 6+1;
-        System.out.println(dice);
+    static final int NO_PLAY = 0;
+    static final int LADDER = 1;
+    static final int SNAKE = 2;
+    static int position = 0;
 
+    public static void main(String[] args) {
+
+        int dice = (int) (Math.random()*10)%6+1;
+        int option = (int)(Math.random()*10)%3;
+
+        if(option == LADDER){
+            System.out.println("Ladder");
+            position+=dice;
+
+        }
+        else if(option == SNAKE){
+            System.out.println("snake");
+            position-=dice;
+
+
+        }
+        else if (option == NO_PLAY) {
+            System.out.println("No Play ");
+        }
     }
 }
+
