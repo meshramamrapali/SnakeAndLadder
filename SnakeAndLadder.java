1af3d5730a3267a20bf8b30cc45f512e5ae99e61
meshramamrapali <amrapalimeshram86@gmail.com>
2023-02-24 21:44:02 +0530

    ADDED : Roll Dice

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,8 +3,9 @@ package SnakeAndLadder;
 public class SnakeAndLadder {
     static final int PLAYER_POSITION = 0;
     public static void main(String[] args) {
+        System.out.println("Welcome to Snake And Ladder Game");
+        int dice = (int) (Math.random() * 10) % 6+1;
+        System.out.println(dice);
 
-        System.out.println("Current Position :" + PLAYER_POSITION);
     }
-
 }
