da167181ec7fd9cef1f1dc33b582aa4e94686762
meshramamrapali <amrapalimeshram86@gmail.com>
2023-02-24 22:25:44 +0530

    ADDED : Winning Position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -9,28 +9,31 @@ public class SnakeAndLadder {
 
     public static void main(String[] args) {
 
-            int dice = (int) (Math.random() * 10) % 6+1;
+        while (position < 100) {
+            int dice = (int) (Math.random() * 10) % 6 + 1;
             int option = (int) (Math.random() * 10) % 3;
 
             if (option == LADDER) {
                 System.out.println("ladder");
-                position +=dice;
+                position += dice;
 
             } else if (option == SNAKE) {
                 System.out.println("snake");
-                position -=dice;
-            }
-            else  if (option == NO_PLAY){
-                System.out.println("N0 play");
-                position = PLAYER_POSITION;
-            }
+                position -= dice;
 
-            if(position<PLAYER_POSITION){
+
+            } else if (option == NO_PLAY) {
+                System.out.println("N0 play");
+                position = position;
+            }
+            if (position < PLAYER_POSITION) {
                 position = 0;
+            }
+            if (position > 100) {
+                position -= dice;
             }
 
             System.out.println("position: " + position);
         }
-
     }
-
+}
